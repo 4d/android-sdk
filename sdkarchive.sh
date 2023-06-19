3cbd0c898bebbcc328f228ea321a06e5be04a7c6
eric.marchand <eric.marchand@4d.com>
2023-06-19 16:49:00 +0200

    archive: allow to launch from another folder

--- sdkarchive.sh
+++ sdkarchive.sh
@@ -4,6 +4,10 @@ name=$1
 if [[ -z "$name" ]]; then
   name="android"
 fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
+
 rm "$name.zip"
 (cd dependencies && zip -r ../$name.zip . )
 ls -l "$name.zip"
